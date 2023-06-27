b67d8e38fb10a583f16f588299231f17e0e05f86
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2023-06-27 10:01:34 +0100

    Update Battledogs.sol

--- Contracts/Battledogs.sol
+++ Contracts/Battledogs.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: No license
 
-// @title NFT Game by OxSorcerers for Arbdoge Fun Gaming
+// @title NFT Game by OxSorcerers for Battledog Games
 // https://twitter.com/0xSorcerers | https://github.com/Dark-Viper | https://t.me/Oxsorcerer | https://t.me/battousainakamoto | https://t.me/darcViper
 
 pragma solidity ^0.8.17;
